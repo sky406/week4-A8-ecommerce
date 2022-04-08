f34c4ce573b6b437b855e8fda50520d29fe5ed27
wesley <wesleynworisa@gmail.com>
2022-04-08 11:15:03 -0700

    changed a the price storage method for products

--- src/app/products/interfaces/product.interface.ts
+++ src/app/products/interfaces/product.interface.ts
@@ -1,7 +1,8 @@
 export interface product
 {
   name:string,
-  price:string,
+  price_d:number,
+  price_c:number
   avalibility:number | string,
   restock_date:Date,
   img:string
@@ -11,9 +12,12 @@ export interface product
 // ladder co
 export function g_product(name:string = 'lorem',price:number = 6.99,num_availible:number = 100,restock_date:Date = new Date, img:string = '',description:string = 'lorem ipsum'):product
 {
+  let p = price.toFixed(2).split('.')
+
   return{
     name:name,
-    price:`$${price.toFixed(2)}`,
+    price_d:Number(p[0]),
+    price_c:Number(p[1]),
     avalibility:num_availible,
     restock_date:restock_date,
     img:img,
